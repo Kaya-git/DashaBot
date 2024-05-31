dca0a095041924ad58accd0d9841fb13b4a487fe
Evgeny Buzuev <ezjell@yandex.ru>
2024-05-31 12:42:23 +0300

    logic

---
+++ src/finalstate/fsm.py
@@ -0,0 +1,7 @@
+from aiogram.fsm.state import StatesGroup, State
+
+
+class GarantStates(StatesGroup):
+    client_telegram_id = State()
+    client_cellphone_num = State()
+    screen_shot = State()
